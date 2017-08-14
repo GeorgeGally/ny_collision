5fe18ddfd30c2eaebccd48bfc442fffe3f44dd9a
george <robopop@radarboy.com>
2017-08-14 14:31:19 -0400

    added preloader

--- js/draw_data.js
+++ js/draw_data.js
@@ -385,6 +385,8 @@ function loadData(){
 		start_date = "";
 		filter();
 		nextTwirlDataPoint();
+		spinner.stop();
+		spinner2.stop();
 	});
 
 }
@@ -1503,3 +1505,33 @@ var page = 1;
 		}
 		});
 	 });
+
+
+
+	 var spin_opts = {
+	   lines: 11 // The number of lines to draw
+	 , length: 25 // The length of each line
+	 , width: 12 // The line thickness
+	 , radius: 34 // The radius of the inner circle
+	 , scale: 0.25 // Scales overall size of the spinner
+	 , corners: 1 // Corner roundness (0..1)
+	 , color: '#000' // #rgb or #rrggbb or array of colors
+	 , opacity: 0.1 // Opacity of the lines
+	 , rotate: 0 // The rotation offset
+	 , direction: 1 // 1: clockwise, -1: counterclockwise
+	 , speed: 2.2 // Rounds per second
+	 , trail: 88 // Afterglow percentage
+	 , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+	 , zIndex: 2e9 // The z-index (defaults to 2000000000)
+	 , className: 'spinner' // The CSS class to assign to the spinner
+	 , top: '50%' // Top position relative to parent
+	 , left: '50%' // Left position relative to parent
+	 , shadow: false // Whether to render a shadow
+	 , hwaccel: false // Whether to use hardware acceleration
+	 , position: 'absolute' // Element positioning
+	 }
+	 var target = document.getElementById('page2')
+	 var spinner = new Spinner(spin_opts).spin(target);
+
+	 var target2 = document.getElementById('page3')
+	 var spinner2 = new Spinner(spin_opts).spin(target);
